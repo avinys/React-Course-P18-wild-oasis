ac88552e3818c22762e3af9428914c9c5268d816
Arvydas Vingis <arvydasvinys@gmail.com>
2025-07-27 12:10:41 +0300

    Connect supabase to the project

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://vwjvbuimhkpswdzwhpxi.supabase.co";
+// The key is safe to expose in the browser as RLS is activated on the database
+const supabaseKey =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3anZidWltaGtwc3dkendocHhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MDM1MjUsImV4cCI6MjA2OTE3OTUyNX0.1uvNIRZ-DpQIlgPxgIbhBbBqm91Krlf_DGfF-6cBgEg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
