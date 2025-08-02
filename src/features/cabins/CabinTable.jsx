a9e9de832a17c4c844acf2af2265434dc7335151
Arvydas Vingis <arvydasvinys@gmail.com>
2025-08-02 20:29:23 +0300

    Create a reusable SortBy component and implement cabins sorting

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -11,12 +11,25 @@ function CabinTable() {
 
 	if (isPending) return <Spinner />;
 
+	// 1) FILTER
 	const filterValue = searchParams.get("discount") || "all";
 	let filteredCabins;
 	if (filterValue === "all") filteredCabins = cabins;
 	else if (filterValue === "no-discount")
 		filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
 	else filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+	// 2) SORT
+	const sortBy = searchParams.get("sortBy") || "name-asc";
+	const [field, direction] = sortBy.split("-");
+	const modifier = direction === "asc" ? 1 : -1;
+	const sortedCabins = filteredCabins.sort(
+		(a, b) => (a[field] - b[field]) * modifier
+		// 	{
+		// 	if (direction === "asc") return a[field] - b[field];
+		// 	else return b[field] - a[field];
+		// }
+	);
 
 	return (
 		<Menus>
@@ -30,7 +43,7 @@ function CabinTable() {
 					<div></div>
 				</Table.Header>
 				<Table.Body
-					data={filteredCabins}
+					data={sortedCabins}
 					render={(cabin) => (
 						<CabinRow cabin={cabin} key={cabin.id} />
 					)}
---
+++ src/ui/SortBy.jsx
@@ -0,0 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+import Select from "./Select";
+
+function SortBy({ options }) {
+	const [searchParams, setSearchParams] = useSearchParams();
+	const sortBy = searchParams.get("sortBy") || "";
+
+	const handleChange = (e) => {
+		searchParams.set("sortBy", e.target.value);
+		setSearchParams(searchParams);
+	};
+	return (
+		<Select
+			options={options}
+			type="jonas"
+			value={sortBy}
+			onChange={handleChange}
+		/>
+	);
+}
+
+export default SortBy;
