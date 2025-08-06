46ab3698d4fe7af22d7fdada3dc35b79051ab35e
Arvydas Vingis <arvydasvinys@gmail.com>
2025-08-06 12:03:29 +0300

    Display chart of sales in dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import { useRecentBookings } from "../../features/dashboard/useRecentBookings";
 import { useRecentStays } from "../../features/dashboard/useRecentStays";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -34,7 +35,7 @@ function DashboardLayout() {
 			/>
 			<div>Today's activity</div>
 			<div>Chart stay durations</div>
-			<div>Chart sales</div>
+			<SalesChart bookings={bookings} numDays={numDays} />
 		</StyledDashboardLayout>
 	);
 }
